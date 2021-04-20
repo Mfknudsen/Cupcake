8ab38cbc7599c3afa4b557d9a6627cb66c112b5c
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-20 16:31:32 +0200

    Added

--- src/main/java/business/services/OrdersFacade.java
+++ src/main/java/business/services/OrdersFacade.java
@@ -1,2 +1,28 @@
-package business.services;public class OrdersFacade {
+package business.services;
+
+import business.entities.Order;
+import business.exceptions.UserException;
+import business.persistence.Database;
+import business.persistence.OrdersMapper;
+
+import java.util.List;
+
+public class OrdersFacade
+{
+    private OrdersMapper ordersMapper;
+
+    public OrdersFacade(Database database)
+    {
+        this.ordersMapper = new OrdersMapper(database);
+    }
+
+    public List<Order> getAllOrders() throws UserException
+    {
+        return ordersMapper.getAllOrders();
+    }
+
+    public List<Order> getOrdersDataByUserId(int userId) throws UserException
+    {
+        return ordersMapper.getOrdersDataByUserId(userId);
+    }
 }
