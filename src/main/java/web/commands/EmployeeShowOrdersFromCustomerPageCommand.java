2157b85036aa18b6fd060dd6d14c9a9a511e8dd9
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-20 16:35:51 +0200

    Added

--- src/main/java/web/commands/EmployeeShowOrdersFromCustomerPageCommand.java
+++ src/main/java/web/commands/EmployeeShowOrdersFromCustomerPageCommand.java
@@ -1,2 +1,31 @@
-package web.commands;public class EmployeeShowOrdersFromCustomerPageCommand {
+package web.commands;
+
+import business.entities.Order;
+import business.exceptions.UserException;
+import business.services.OrdersFacade;
+import business.services.UserFacade;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.util.List;
+
+public class EmployeeShowOrdersFromCustomerPageCommand extends CommandProtectedPage
+{
+    OrdersFacade ordersFacade;
+    UserFacade userFacade;
+
+    public EmployeeShowOrdersFromCustomerPageCommand(String pageToShow, String role) {
+        super(pageToShow, role);
+        this.ordersFacade = new OrdersFacade(database);
+        this.userFacade = new UserFacade(database);
+    }
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
+        String userId = request.getParameter("userId");
+
+        List<Order> listOfOrdersByUserId = ordersFacade.getOrdersDataByUserId(Integer.parseInt(userId));
+        request.setAttribute("listOfOrdersByUserId", listOfOrdersByUserId);
+        return pageToShow;
+    }
 }
