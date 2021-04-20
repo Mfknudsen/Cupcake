1492fa1d7a27049027e341227975668c0906c576
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-20 16:28:55 +0200

    Added

--- src/main/java/business/entities/Order.java
+++ src/main/java/business/entities/Order.java
@@ -1,2 +1,73 @@
-package business.entities;public class Order {
+package business.entities;
+
+import java.util.Date;
+
+public class Order
+{
+    private int id, topping, bottom;
+    private float price;
+    private String userName;
+    private Date date;
+
+    public Order(int id, String userName, Date date) {
+       this.id = id;
+       this.userName = userName;
+       this.date = date;
+    }
+
+    public Order(int id, int topping, int bottom, float price, Date date) {
+       this.id = id;
+       this.topping = topping;
+       this.bottom = bottom;
+       this.price = price;
+       this.date = date;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getTopping() {
+        return topping;
+    }
+
+    public void setTopping(int topping) {
+        this.topping = topping;
+    }
+
+    public int getBottom() {
+        return bottom;
+    }
+
+    public void setBottom(int bottom) {
+        this.bottom = bottom;
+    }
+
+    public float getPrice() {
+        return price;
+    }
+
+    public void setPrice(float price) {
+        this.price = price;
+    }
+
+    public String getUserName() {
+        return userName;
+    }
+
+    public void setUserName(String userName) {
+        this.userName = userName;
+    }
+
+    public Date getDate() {
+        return date;
+    }
+
+    public void setDate(Date date) {
+        this.date = date;
+    }
 }
