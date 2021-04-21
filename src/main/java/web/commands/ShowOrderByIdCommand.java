0d9d7f996f28e29e753217ee41ed9e7bc7f876e1
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-21 13:16:36 +0200

    Renamed + updated

--- src/main/java/web/commands/ShowOrderByIdCommand.java
+++ src/main/java/web/commands/ShowOrderByIdCommand.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class EmployeeShowOrderByIdCommand extends CommandProtectedPage
+public class ShowOrderByIdCommand extends CommandProtectedPage
 {
     OrdersFacade ordersFacade;
     UserFacade userFacade;
 
-    public EmployeeShowOrderByIdCommand(String pageToShow, String role) {
+    public ShowOrderByIdCommand(String pageToShow, String role) {
         super(pageToShow, role);
         this.ordersFacade = new OrdersFacade(database);
         this.userFacade = new UserFacade(database);
@@ -25,7 +25,12 @@ public class EmployeeShowOrderByIdCommand extends CommandProtectedPage
         int userId = Integer.parseInt(request.getParameter("userId"));
         int orderId = Integer.parseInt(request.getParameter("orderId"));
 
-        List<Order> listOrderByOrderId = ordersFacade.getOrder(userId, orderId);
+        List<Order> specificOrder = ordersFacade.getOrder(userId, orderId);
+        if (!(specificOrder.size() > 0))
+        {
+            request.setAttribute("error", "Could not retrieve any orders, possible problem could be that the order or user does not exist anymore.");
+        }
+        request.setAttribute("specificOrder", specificOrder);
         return pageToShow;
     }
 }
