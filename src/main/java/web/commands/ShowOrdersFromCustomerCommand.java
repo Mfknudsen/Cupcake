23341b870a979212ce7f88db5f05b958477f4c05
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-21 13:17:17 +0200

    Renamed + updated

--- src/main/java/web/commands/ShowOrdersFromCustomerCommand.java
+++ src/main/java/web/commands/ShowOrdersFromCustomerCommand.java
@@ -9,12 +9,12 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class ShowOrdersFromCustomerPageCommand extends CommandProtectedPage
+public class ShowOrdersFromCustomerCommand extends CommandProtectedPage
 {
     OrdersFacade ordersFacade;
     UserFacade userFacade;
 
-    public ShowOrdersFromCustomerPageCommand(String pageToShow, String role) {
+    public ShowOrdersFromCustomerCommand(String pageToShow, String role) {
         super(pageToShow, role);
         this.ordersFacade = new OrdersFacade(database);
         this.userFacade = new UserFacade(database);
@@ -24,12 +24,12 @@ public class ShowOrdersFromCustomerPageCommand extends CommandProtectedPage
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
         int userId = Integer.parseInt(request.getParameter("userId"));
 
-        List<Order> listOfOrdersByUserId = ordersFacade.getOrdersDataByUserId(userId);
-        if (!(listOfOrdersByUserId.size() > 0))
+        List<Order> ordersFromUser = ordersFacade.getOrders(userId);
+        if (!(ordersFromUser.size() > 0))
         {
-            request.setAttribute("error", "Could not receive any orders, perhaps this user has not ordered anything yet.");
+            request.setAttribute("error", "Could not retrieve any orders, perhaps this user has not ordered anything yet.");
         }
-        request.setAttribute("listOfOrdersByUserId", listOfOrdersByUserId);
+        request.setAttribute("ordersFromUser", ordersFromUser);
         return pageToShow;
     }
 }
