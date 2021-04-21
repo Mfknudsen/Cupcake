f61ba7cf4aeca8d18ed002adad2b2c5e9f4ae180
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-21 10:38:33 +0200

    Updated

--- src/main/java/web/commands/EmployeeShowOrderByIdCommand.java
+++ src/main/java/web/commands/EmployeeShowOrderByIdCommand.java
@@ -1,7 +1,28 @@
 package web.commands;
 
-public class EmployeeShowOrderByIdCommand extends Command {
-    public EmployeeShowOrderByIdCommand(String employeeshoworderbyid, String employee) {
-        super();
+import business.exceptions.UserException;
+import business.services.OrdersFacade;
+import business.services.UserFacade;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class EmployeeShowOrderByIdCommand extends CommandProtectedPage
+{
+    OrdersFacade ordersFacade;
+    UserFacade userFacade;
+
+    public EmployeeShowOrderByIdCommand(String pageToShow, String role) {
+        super(pageToShow, role);
+        this.ordersFacade = new OrdersFacade(database);
+        this.userFacade = new UserFacade(database);
+    }
+
+    @Override
+    public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
+        String userId = request.getParameter("userList");
+
+
+        return pageToShow;
     }
 }
