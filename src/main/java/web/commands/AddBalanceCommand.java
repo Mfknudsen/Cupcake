c56ae8239963df85f5e9e2fc70b22fdb6230e177
Zero-slayer <71024460+Zero-slayer@users.noreply.github.com>
2021-04-23 10:23:54 +0200

    Updated

--- src/main/java/web/commands/AddBalanceCommand.java
+++ src/main/java/web/commands/AddBalanceCommand.java
@@ -9,28 +9,27 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import java.util.List;
 
-public class ShowOrderByIdCommand extends CommandProtectedPage
+public class AddBalanceCommand extends CommandProtectedPage
 {
-    OrdersFacade ordersFacade;
     UserFacade userFacade;
 
-    public ShowOrderByIdCommand(String pageToShow, String role) {
+    public AddBalanceCommand(String pageToShow, String role) {
         super(pageToShow, role);
-        this.ordersFacade = new OrdersFacade(database);
         this.userFacade = new UserFacade(database);
     }
 
     @Override
     public String execute(HttpServletRequest request, HttpServletResponse response) throws UserException {
         int userId = Integer.parseInt(request.getParameter("userId"));
-        int orderId = Integer.parseInt(request.getParameter("orderId"));
+        float amount = Float.parseFloat(request.getParameter("amount"));
 
-        List<Order> specificOrder = ordersFacade.getOrder(userId, orderId);
-        if (!(specificOrder.size() > 0))
+        int affectedRows = userFacade.addBalance(amount, userId);
+        if (!(affectedRows > 0))
         {
-            request.setAttribute("error", "Could not retrieve any orders, possible problem could be that the order or user does not exist anymore.");
+            request.setAttribute("error", "Could not find the user");
+            return pageToShow;
         }
-        request.setAttribute("specificOrder", specificOrder);
+        request.setAttribute("addBalanceSuccess", "Selected user now has: " + userFacade.getBalance(userId));
         return pageToShow;
     }
 }
